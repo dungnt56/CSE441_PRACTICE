98840687a715c6a9cf60bc9e2661c9dd4eb741ca
DungNt <tandung.nguyen@migitek.com>
2024-09-21 21:38:34 +0700

    Ex1: ui + logic

--- Ex01/app/src/main/java/com/dungnt/ex01/MainActivity.java
+++ Ex01/app/src/main/java/com/dungnt/ex01/MainActivity.java
@@ -1,6 +1,10 @@
 package com.dungnt.ex01;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,48 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtA, edtB, edtKQ;
+    Button btncong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btncong = findViewById(R.id.btntong);
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isValidNumber(edtA.getText().toString())){
+                    edtA.setError("So khong hop le");
+                    return;
+                }
+                if (!isValidNumber(edtB.getText().toString())){
+                    edtB.setError("So khong hop le");
+                    return;
+                }
+
+
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                int c = a + b;
+                edtKQ.setText(c+"");
+            }
         });
     }
+    public Boolean isValidNumber(String value){
+
+        if (TextUtils.isEmpty(value)) {
+            return false;
+        }
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
 }
