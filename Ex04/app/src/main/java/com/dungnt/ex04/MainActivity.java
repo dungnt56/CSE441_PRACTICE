4f6a696c45e524d654ed6565ebfb6fe88849ec7e
DungNt <tandung.nguyen@migitek.com>
2024-09-23 16:35:11 +0700

    Ex4: task 1 - convert temporary

--- Ex04/app/src/main/java/com/dungnt/ex04/MainActivity.java
+++ Ex04/app/src/main/java/com/dungnt/ex04/MainActivity.java
@@ -1,6 +1,10 @@
 package com.dungnt.ex04;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCalculatorBmi;
+    Button btnConvertTemporary;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        btnCalculatorBmi = findViewById(R.id.btnCalculatorBMI);
+        btnConvertTemporary = findViewById(R.id.btnConvertTemporary);
+
+        btnConvertTemporary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inten = new Intent(MainActivity.this, ConvertTemporary.class);
+                startActivity(inten);
+
+            }
+        });
+
+        btnCalculatorBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Open calculator BMI", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
