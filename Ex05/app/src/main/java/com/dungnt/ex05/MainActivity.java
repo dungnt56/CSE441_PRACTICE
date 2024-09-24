69213ef283c91fb4456723747c5d44f22f24be2d
DungNt <tandung.nguyen@migitek.com>
2024-09-25 06:44:57 +0700

    Ex05: dev logic convert year

--- Ex05/app/src/main/java/com/dungnt/ex05/MainActivity.java
+++ Ex05/app/src/main/java/com/dungnt/ex05/MainActivity.java
@@ -8,8 +8,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final Map<Integer, String> MAP_CAN;
+
+    static {
+        MAP_CAN = new HashMap<>();
+        MAP_CAN.put(0, "Canh");
+        MAP_CAN.put(1, "Tân");
+        MAP_CAN.put(2, "Nhâm");
+        MAP_CAN.put(3, "Quý");
+        MAP_CAN.put(4, "Giáp");
+        MAP_CAN.put(5, "Ất");
+        MAP_CAN.put(6, "Bính");
+        MAP_CAN.put(7, "Đinh");
+        MAP_CAN.put(8, "Mậu");
+        MAP_CAN.put(9, "Kỷ");
+    }
+
+    public static final Map<Integer, String> MAP_CHI;
+    static {
+        MAP_CHI = new HashMap<>();
+        MAP_CHI.put(0, "Thân");
+        MAP_CHI.put(1, "Dậu");
+        MAP_CHI.put(2, "Tuất");
+        MAP_CHI.put(3, "Hợi");
+        MAP_CHI.put(4, "Tý");
+        MAP_CHI.put(5, "Sửu");
+        MAP_CHI.put(6, "Dần");
+        MAP_CHI.put(7, "Mẹo");
+        MAP_CHI.put(8, "Thìn");
+        MAP_CHI.put(9, "Tỵ");
+        MAP_CHI.put(10, "Ngọ");
+        MAP_CHI.put(11, "Mùi");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +55,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    private String convertYearNumberToChinaYear(String yearNumber){
+        if (!isValidYear(yearNumber)){
+            return "Nam khong hop le";
+        }
+        Integer yearValue = Integer.parseInt(yearNumber);
+        String can = MAP_CAN.get(yearValue % 10);
+        String am = MAP_CHI.get(yearValue % 12);
+        return can + " " + am;
+    }
+
+    /*
+        * Description: check valid year number
+        * Condition:
+            - only has number character
+            - Number of character is 4
+            - Bigger than 0 and smaller or equal 9999
+    */
+    public boolean isValidYear(String year) {
+        if (year == null || year.length() != 4 || !year.matches("\\d+")) {
+            return false;
+        }
+        int yearValue = Integer.parseInt(year);
+        return yearValue > 0 && yearValue <= 9999;
     }
 }
