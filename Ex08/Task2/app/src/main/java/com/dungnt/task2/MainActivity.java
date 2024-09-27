2e4c2b320af3b5a6f712654b8c6081ca806c0527
DungNt <tandung.nguyen@migitek.com>
2024-09-27 17:44:57 +0700

    Ex08: task 2 - Capture

--- Ex08/Task2/app/src/main/java/com/dungnt/task2/MainActivity.java
+++ Ex08/Task2/app/src/main/java/com/dungnt/task2/MainActivity.java
@@ -1,19 +1,57 @@
 package com.dungnt.task2;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
 
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton btnCapture;
+    ImageView ivImg;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ivImg = findViewById(R.id.iv_img);
+        btnCapture = findViewById(R.id.btn_capture);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ACTION_IMAGE_CAPTURE);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                {
+                    ActivityCompat.requestPermissions(MainActivity.this,new
+                            String[]{android.Manifest.permission.CAMERA}, 1);
+                    return;
+                }
+                startActivityForResult(intent,99);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 99 && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ivImg.setImageBitmap(photo);
+        }
     }
 }
