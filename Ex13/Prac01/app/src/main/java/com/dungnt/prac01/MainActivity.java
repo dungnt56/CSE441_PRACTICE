24b035e0e6b056f0cfb24b62defafccf04ff4276
DungNt <tandung.nguyen@migitek.com>
2024-10-21 19:01:05 +0700

    Ex13: prac01

--- Ex13/Prac01/app/src/main/java/com/dungnt/prac01/MainActivity.java
+++ Ex13/Prac01/app/src/main/java/com/dungnt/prac01/MainActivity.java
@@ -1,24 +1,45 @@
 package com.dungnt.prac01;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView selection;
+    AutoCompleteTextView singleComplete;
+    MultiAutoCompleteTextView multiComplete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        selection = findViewById(R.id.selection);
+
+        String[] arr = {"Hà Nội", "Huế", "Sài Gòn", "Hà Giang", "Hội An", "Kiên Giang", "Lâm Đồng", "Long Khánh"};
+
+        singleComplete = findViewById(R.id.edtauto);
+        ArrayAdapter<String> myadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arr);
+        singleComplete.setAdapter(myadapter);
+
+        multiComplete = findViewById(R.id.multiAutoCompleteTextView);
+        multiComplete.setAdapter(myadapter);
+        multiComplete.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+        singleComplete.addTextChangedListener(new android.text.TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                selection.setText(singleComplete.getText());
+            }
+
+            @Override
+            public void afterTextChanged(android.text.Editable editable) { }
         });
     }
 }
