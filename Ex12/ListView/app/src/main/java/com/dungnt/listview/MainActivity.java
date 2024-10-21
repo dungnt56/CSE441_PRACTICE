526c818408d93e3b1a813f50a0feea1088a697e2
DungNt <tandung.nguyen@migitek.com>
2024-10-21 18:23:05 +0700

    Ex12: list view

--- Ex12/ListView/app/src/main/java/com/dungnt/listview/MainActivity.java
+++ Ex12/ListView/app/src/main/java/com/dungnt/listview/MainActivity.java
@@ -1,6 +1,11 @@
 package com.dungnt.listview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        txtl = findViewById(R.id.selection);
+
+        String[] arr1 = {"Iphone 7", "SamSung Galaxy S7", "Nokia Lumia 730",
+                "Sony Xperia XZ", "HTC One E9",
+                "Sony Xperia XZ", "HTC One E9",
+                "Sony Xperia XZ", "HTC One E9",
+                "Sony Xperia XZ", "HTC One E9",
+                "Sony Xperia XZ", "HTC One E9",
+                "Sony Xperia XZ", "HTC One E9",
+                "Sony Xperia XZ", "HTC One E9",
+                "Sony Xperia XZ", "HTC One E9",
+                "Sony Xperia XZ", "HTC One E9"
+        };
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arr1);
+        ListView lv1 = findViewById(R.id.lv1);
+        lv1.setAdapter(adapter1);
+
+        lv1.setOnItemClickListener((arg0, arg1, i, arg3) -> txtl.setText("Vị trí " + i + " : " + arr1[i]));
     }
 }
