01c6b0e555fa385685f23d1f1bbd295caf5cad52
DungNt <tandung.nguyen@migitek.com>
2024-10-22 11:06:14 +0700

    Ex14: tabselector

--- Ex14/TabSelector/app/src/main/java/com/dungnt/tabselector/MainActivity.java
+++ Ex14/TabSelector/app/src/main/java/com/dungnt/tabselector/MainActivity.java
@@ -1,24 +1,69 @@
 package com.dungnt.tabselector;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TabHost;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edta, edtb;
+    Button btncong;
+    ListView lv1;
+    ArrayList<String> list;
+    ArrayAdapter<String> myarray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        addControl();
+        addEvent();
+    }
+
+    private void addControl() {
+        TabHost tab = findViewById(R.id.tabhost);
+        tab.setup();
+
+        TabHost.TabSpec tab1 = tab.newTabSpec("t1");
+        tab1.setContent(R.id.tab1);
+        tab1.setIndicator("Phép cộng");
+        tab.addTab(tab1);
+
+        TabHost.TabSpec tab2 = tab.newTabSpec("t2");
+        tab2.setContent(R.id.tab2);
+        tab2.setIndicator("Lịch sử");
+        tab.addTab(tab2);
+        tab1.setIndicator("Phép cộng", getResources().getDrawable(R.drawable.ic_plus));
+        tab2.setIndicator("Lịch sử", getResources().getDrawable(R.drawable.ic_history));
+
+        edta = findViewById(R.id.edta);
+        edtb = findViewById(R.id.edtb);
+        btncong = findViewById(R.id.btncong);
+        lv1 = findViewById(R.id.lv1);
+        list = new ArrayList<>();
+        myarray = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        lv1.setAdapter(myarray);
+    }
+
+    private void addEvent() {
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edta.getText().toString());
+                int b = Integer.parseInt(edtb.getText().toString());
+                String c = a + " + " + b + " = " + (a + b);
+                list.add(c);
+                myarray.notifyDataSetChanged();
+                edta.setText("");
+                edtb.setText("");
+            }
         });
     }
 }
